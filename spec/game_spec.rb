7043f03f40a49014ff6ac38979e4cc04bec04ebc
Max Beizer <max.beizer@gmail.com>
2015-05-25 16:35:17 -0500

    Add list of methods in Game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,4 +3,13 @@ require_relative '../lib/pennies'
 require 'pry'
 
 RSpec.describe Game do
+  # initialize
+  # play!
+  # return_the_winner
+  # play_human_turn
+  # play_computer_turn
+  # collect_input
+  # input_invalid?
+  # invalid_input
+  # annotate_game
 end
